d18d2c3f1d5553a1d01c06c624f2cd6265a0fbb9
adam-ubuntu <gy.adam90@gmail.com>
2020-04-20 18:19:39 -0700

    Add starter code

---
+++ build/precache-manifest.8f0425f672988c697dc6230731f6a123.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f01318c4024a36b6008e7933b5845b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a59b58a12ceac1aa626",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "3684ed3cf2c5d26ef507",
+    "url": "/static/js/2.aa438d0a.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/static/js/2.aa438d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a59b58a12ceac1aa626",
+    "url": "/static/js/main.870a1d56.chunk.js"
+  },
+  {
+    "revision": "50e539a6637833689ed0",
+    "url": "/static/js/runtime-main.f0c161ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
