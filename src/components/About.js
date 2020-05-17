8351dbb51ca9beb95d2f5619ee3d2855b7a4bd74
adam-ubuntu <gy.adam90@gmail.com>
2020-05-17 12:46:11 -0700

    Update text & layout on About page

--- src/components/About.js
+++ src/components/About.js
@@ -10,12 +10,6 @@ const About = () => {
                   This website was created for the  <a href="https://udacity.com">Full Stack Developer Nanodegree at Udacity</a>.
                 </p>
                 <p>
-                  Both the frontend and the backend are hosted on Heroku, and their source code is available on <a href="https://github.com/gyadam/quizoverflow">GitHub</a>.
-                </p>
-                <p>
-                  Although a very simple website, I learned a lot while building it and really enjoyed the process, as I had no prior experience in web development at all.
-                </p>
-                <p>
                   The goal of the project was to:
                   <ul>
                     <li>Create a backend API following RESTful principles</li>
@@ -24,8 +18,18 @@ const About = () => {
                     <li>Use Auth0 to authenticate users with JSON web tokens (JWTs)</li>
                     <li>Use role-based access control (RBAC) to authorize users to make API calls</li>
                     <li>Apply test-driven development, writing unit tests for each endpoint</li>
-                    <li>Deploy the backend (and frontend) application to Heroku</li>
+                    <li>Deploy the backend and frontend to Heroku</li>
                   </ul>
+                  <p>
+                  Both the frontend and the backend are hosted on Heroku, and their source code is available on <a href="https://github.com/gyadam/quizoverflow">GitHub</a>.
+                </p>
+                <p>
+                  The website allows visitors to play categorized quizzes consisting of 5 questions in the selected category.
+                  Visitors can also view the list of questions, but can only view answers and edit/delete the questions if they are logged in and have the right permissions, which are controlled through <a href="https://auth0.com">Auth0</a>.
+                </p>
+                <p>
+                  Although a very simple website, I learned a lot while building it and really enjoyed the process, as I had no prior experience in web development at all.
+                </p>
                   Tech Stack:
                   <ul>
                     <li><b>SQLAlchemy ORM</b> as the ORM library</li>
